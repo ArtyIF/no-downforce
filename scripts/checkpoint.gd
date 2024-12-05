784a54c53c7c8e22431ec1393edf2f2a9de8203e
ArtyIF <artyomisflash@mail.ru>
2024-12-06 00:35:02 +0300

    Turns out those signals don't get exported when set through the editor

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -2,6 +2,9 @@ extends Area3D
 
 @export var next: Node3D
 
-func _on_body_entered(body: Node3D) -> void:
+func _ready() -> void:
+	body_entered.connect(on_body_entered)
+
+func on_body_entered(body: Node3D) -> void:
 	if body is Car and AACCGlobal.current_car == body:
 		NoDownforceGlobal.activate_next_checkpoint(self, next)
