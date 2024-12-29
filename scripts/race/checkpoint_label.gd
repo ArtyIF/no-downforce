8146e7748709e731e815de6664bd86c16cc6cea2
ArtyIF <artyomisflash@mail.ru>
2024-12-29 23:46:11 +0300

    0.6-pre10 - new checkpoints

--- scripts/race/checkpoint_label.gd
+++ scripts/race/checkpoint_label.gd
@@ -1,4 +1,11 @@
 extends Label
 
+@export var at_checkpoint: bool = false
+
 func _process(_delta: float) -> void:
-	text = "%d/5" % NoDownforceGlobal.checkpoints_passed
+	if at_checkpoint and NoDownforceGlobal.checkpoints_passed + 1 == NoDownforceGlobal.total_checkpoints:
+		text = "FINISH"
+		$"../CheckpointLabel".visible = false
+	else:
+		text = "%d/%d" % [NoDownforceGlobal.checkpoints_passed + (1 if at_checkpoint else 0), NoDownforceGlobal.total_checkpoints]
+		$"../CheckpointLabel".visible = true
