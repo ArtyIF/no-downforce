0fe4714f52a6d369dd12e2e96980fb9f5a715353
ArtyIF <artyomisflash@mail.ru>
2024-12-30 13:13:56 +0300

    0.6-pre20 - fixed target time showing when vs ghost->solo (since 0.5.4)

--- scripts/ui/menu/main_menu_button.gd
+++ scripts/ui/menu/main_menu_button.gd
@@ -4,9 +4,9 @@ func _ready() -> void:
 	pressed.connect(on_press)
 
 func on_press() -> void:
+	NoDownforceGlobal.demo_car_input.demo = null
 	NoDownforceGlobal.race_tracker.reset(true)
 	NoDownforceGlobal.ui_manager.hide_overlay("DemoOverlay")
 	NoDownforceGlobal.ui_manager.show_screen("MainMenu")
 	get_tree().paused = false
-	NoDownforceGlobal.demo_car_input.demo = null
 	NoDownforceGlobal.ui_manager.screens["MainMenu"].get_node("BG/HBox/RaceButton").call_deferred("grab_focus")
