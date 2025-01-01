de31b5d159ae513991fe46459b3cac4c86c1cd13
ArtyIF <artyomisflash@mail.ru>
2025-01-01 14:47:00 +0300

    0.6.4-pre3 - fix auto-exposure, add option in settings

--- scripts/ui/settings/auto_exposure_setting.gd
+++ scripts/ui/settings/auto_exposure_setting.gd
@@ -7,14 +7,16 @@ func _ready() -> void:
 	on_value_changed(NoDownforceGlobal.settings_resource.graphics_auto_exposure)
 
 func on_value_changed(index: int):
+	var env: Environment = world_env.environment
 	var attr: CameraAttributesPractical = world_env.camera_attributes
 	match index:
 		0:
-			attr.exposure_multiplier = 0.5
+			env.tonemap_exposure = 0.5
 			attr.auto_exposure_enabled = false
 		1:
-			attr.exposure_multiplier = 1.0
+			env.tonemap_exposure = 1.0
 			attr.auto_exposure_enabled = true
+	world_env.environment = env
 	world_env.camera_attributes = attr
 	
 	NoDownforceGlobal.settings_resource.graphics_auto_exposure = index
