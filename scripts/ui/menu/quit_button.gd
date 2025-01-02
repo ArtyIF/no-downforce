dd4bb9caa4e25e97e96576a2a097ebd4acd3227e
ArtyIF <artyomisflash@mail.ru>
2025-01-02 16:04:02 +0300

    0.6.5 - near-complete web support (the sun's too bright)

--- scripts/ui/menu/quit_button.gd
+++ scripts/ui/menu/quit_button.gd
@@ -3,6 +3,7 @@ extends Button
 func _ready() -> void:
 	pressed.connect(on_press)
 	get_tree().set_auto_accept_quit(false)
+	visible = not OS.has_feature("web")
 
 func on_press() -> void:
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
--- scripts/ui/version_label.gd
+++ scripts/ui/version_label.gd
@@ -2,7 +2,7 @@ extends RichTextLabel
 
 func _ready() -> void:
 	meta_clicked.connect(on_meta_clicked)
-	set_deferred("text", "Version %s (Checking updates...)" % ProjectSettings.get_setting("application/config/version"))
+	set_deferred("text", "Version %s\nChecking updates..." % ProjectSettings.get_setting("application/config/version"))
 
 	if not OS.has_feature("editor"):
 		var platform: String = ""
@@ -10,23 +10,26 @@ func _ready() -> void:
 			platform = "linux"
 		elif OS.has_feature("windows"):
 			platform = "win"
+		elif OS.has_feature("web"):
+			set_deferred("text", "Version %s\nDownload the standalone version below for a smoother experience!" % ProjectSettings.get_setting("application/config/version"))
+			return
 
 		var req: HTTPRequest = $VersionRequest
 		req.request_completed.connect(update_check)
 		req.request("https://itch.io/api/1/x/wharf/latest?game_id=3159377&channel_name=%s-x64" % platform)
 	else:
-		set_deferred("text", "Version %s (Running in editor, no update check)" % ProjectSettings.get_setting("application/config/version"))
+		set_deferred("text", "Version %s\nRunning from editor" % ProjectSettings.get_setting("application/config/version"))
 
 func update_check(result, response_code, _headers, body):
 	if result == HTTPRequest.RESULT_SUCCESS and response_code < 400:
 		var json = JSON.parse_string(body.get_string_from_utf8())
 		var version = json["latest"]
 		if version == ProjectSettings.get_setting("application/config/version"):
-			set_deferred("text", "Version %s (Up to date)" % ProjectSettings.get_setting("application/config/version"))
+			set_deferred("text", "Version %s\nUp to date" % ProjectSettings.get_setting("application/config/version"))
 		else:
-			set_deferred("text", "Version %s (New version available! %s)\nDownload at [url]https://artyif.itch.io/no-downforce[/url]" % [ProjectSettings.get_setting("application/config/version"), version])
+			set_deferred("text", "Version %s\nNew version available! %s\nUpdate now: [url]https://artyif.itch.io/no-downforce[/url]" % [ProjectSettings.get_setting("application/config/version"), version])
 	else:
-		set_deferred("text", "Version %s (Update check failed!)" % ProjectSettings.get_setting("application/config/version"))
+		set_deferred("text", "Version %s\nUpdate check failed!" % ProjectSettings.get_setting("application/config/version"))
 
 func on_meta_clicked(meta):
 	OS.shell_open(str(meta))
