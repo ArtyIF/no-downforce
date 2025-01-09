02e975769d6e4c630ed4bdcef60f24f98f510278
ArtyIF <artyomisflash@mail.ru>
2025-01-09 03:00:53 +0300

    0.6.10pre1 - added quit button back on web

--- scripts/ui/menu/quit_button.gd
+++ scripts/ui/menu/quit_button.gd
@@ -3,7 +3,6 @@ extends Button
 func _ready() -> void:
 	pressed.connect(on_press)
 	get_tree().set_auto_accept_quit(false)
-	visible = not OS.has_feature("web")
 
 func on_press() -> void:
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
