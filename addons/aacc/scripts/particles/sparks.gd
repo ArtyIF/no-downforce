bc3f08e15452b3333a5d8cd8eb674537226f4949
ArtyIF <artyomisflash@mail.ru>
2025-01-17 01:59:25 +0300

    1.0.1 - fuck

--- addons/aacc/scripts/particles/sparks.gd
+++ addons/aacc/scripts/particles/sparks.gd
@@ -1,5 +1,8 @@
 extends GPUParticles3D
 
+var emitted: bool = false
+
 func _ready() -> void:
 	emitting = true
-	finished.connect(queue_free)
+	await get_tree().create_timer(1.0).timeout
+	queue_free()
