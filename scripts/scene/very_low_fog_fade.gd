f32c8056eaaad6dde1eb896d1964a6eac136aa83
ArtyIF <artyomisflash@mail.ru>
2025-02-02 13:44:13 +0300

    1.1.0-pre12 - new boot image

--- scripts/scene/very_low_fog_fade.gd
+++ scripts/scene/very_low_fog_fade.gd
@@ -8,5 +8,5 @@ func _process(delta: float) -> void:
 	env.fog_density = lerp(0.0025, 0.2, fade_amount.get_current_value())
 	env.fog_height_density = lerp(0.5, 0.0, fade_amount.get_current_value())
 	env.fog_sky_affect = lerp(0.25, 0.99, fade_amount.get_current_value())
-	env.fog_sun_scatter = lerp(0.025, 0.025, fade_amount.get_current_value())
+	env.fog_sun_scatter = lerp(0.02, 0.01, fade_amount.get_current_value())
 	environment = env
