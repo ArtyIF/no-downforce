bcfa167f98a27bcf8f25b9775d0ef3c2572a8902
ArtyIF <artyomisflash@mail.ru>
2025-02-12 21:54:04 +0300

    1.1.0-post2 - Godot 4.4b3, update lightmap, improve sound

---
+++ addons/aacc/scripts/audio/car_tire_roll_sound.gd
@@ -0,0 +1,21 @@
+## TODO: documentation
+class_name CarTireRollSound extends AudioStreamPlayer3D
+
+@onready var car: Car = get_node("..")
+
+var smooth_volume: SmoothedFloat = SmoothedFloat.new(0.0, 50.0, 5.0)
+
+func _physics_process(delta: float) -> void:
+	var target_value: float = abs(car.local_linear_velocity.z / 15.0)
+	smooth_volume.advance_to(clamp(target_value * car.ground_coefficient, 0.0, 1.0), delta)
+	volume_db = linear_to_db(smooth_volume.get_current_value())
+	
+	if target_value > 0.01:
+		pitch_scale = clamp(target_value, 0.01, 4.0)
+
+	if is_inf(volume_db) or car.freeze:
+		volume_db = -80.0
+	if volume_db >= -60.0 and not playing:
+		play(randf_range(0.0, stream.get_length()))
+	elif volume_db < -60.0 and playing:
+		stop()
