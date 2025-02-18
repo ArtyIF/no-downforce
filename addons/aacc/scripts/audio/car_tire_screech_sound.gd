6a9cbcb8207352898fbb230c5b852c875a063f7c
ArtyIF <artyomisflash@mail.ru>
2025-02-18 22:55:51 +0300

    1.3.0-pre8 - new landing sound

--- addons/aacc/scripts/audio/car_tire_screech_sound.gd
+++ addons/aacc/scripts/audio/car_tire_screech_sound.gd
@@ -1,14 +1,28 @@
 ## TODO: documentation
 class_name CarTireScreechSound extends AudioStreamPlayer3D
 @export var pitch_range: Vector2 = Vector2(0.5, 1.0)
+@export var hit_sound: PackedScene
 
 @onready var car: Car = get_node("..")
-var smooth_burnout_amount = SmoothedFloat.new(0.0, 4.0, 4.0)
+var smooth_burnout_amount = SmoothedFloat.new(0.0, 8.0, 8.0)
+var smooth_burnout_amount_land = SmoothedFloat.new(0.0, 8.0, 8.0)
+var old_ground_coefficient: float = 1.0
 
 func _physics_process(delta: float) -> void:
 	smooth_burnout_amount.advance_to(clamp(car.burnout_amount * 10.0, 0.0, 1.0), delta)
-	pitch_scale = lerp(pitch_range.x, pitch_range.y, smooth_burnout_amount.get_current_value())
-	volume_db = linear_to_db(clamp(car.burnout_amount * 10.0, 0.0, 1.0))
+
+	smooth_burnout_amount_land.advance_to(0.0, delta)
+	if is_zero_approx(old_ground_coefficient) and car.ground_coefficient > 0.0:
+		var land_velocity: float = clamp(abs(car.old_linear_velocity.y) / 10.0, 0.0, 1.0)
+		smooth_burnout_amount_land.force_current_value(land_velocity)
+		var hit_instance: AudioStreamPlayer3D = hit_sound.instantiate()
+		hit_instance.volume_db = linear_to_db(land_velocity)
+		hit_instance.pitch_scale = randf_range(0.9, 1.1)
+		add_child(hit_instance)
+	
+	var burnout_land: float = max(smooth_burnout_amount.get_current_value(), smooth_burnout_amount_land.get_current_value())
+	pitch_scale = 1.0 if burnout_land > smooth_burnout_amount.get_current_value() else lerp(pitch_range.x, pitch_range.y, smooth_burnout_amount.get_current_value())
+	volume_db = linear_to_db(clamp(burnout_land * 10.0, 0.0, 1.0))
 
 	if is_inf(volume_db) or car.freeze:
 		volume_db = -80.0
@@ -16,3 +30,5 @@ func _physics_process(delta: float) -> void:
 		play(randf_range(0.0, stream.get_length()))
 	elif volume_db < -60.0 and playing:
 		stop()
+	
+	old_ground_coefficient = car.ground_coefficient
