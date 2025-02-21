d8faace525b3038f9f9b5964f8750e5e4697e1af
ArtyIF <artyomisflash@mail.ru>
2025-02-21 14:52:11 +0300

    1.3.0-pre17 - small stuff

--- scripts/scene/reflection_probe.gd
+++ scripts/scene/reflection_probe.gd
@@ -1,4 +1,4 @@
 extends ReflectionProbe
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	global_position = AACCGlobal.current_car.global_position + (AACCGlobal.current_car.global_basis.y * 0.25)
