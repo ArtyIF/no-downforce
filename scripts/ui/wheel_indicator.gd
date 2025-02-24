a484e5eeb3345ae777f1dcc33688ab3f7d58a1b8
ArtyIF <artyomisflash@mail.ru>
2025-02-24 23:34:50 +0300

    1.3.1-post2 - webgl fix

--- scripts/ui/wheel_indicator.gd
+++ scripts/ui/wheel_indicator.gd
@@ -1,5 +1,5 @@
-extends ColorRect
+extends Control
 @export var node_path: String
 
 func _process(delta: float) -> void:
-	visible = AACCGlobal.current_car.get_node(node_path).is_colliding
+	modulate = Color.WHITE if AACCGlobal.current_car.get_node(node_path).is_colliding else Color.TRANSPARENT
